e51ba6e4c242d70433635c8d504b61c9ccf5fcfd
Nithin Aradhya <nithin.aradhya@lirisoft.com>
2021-06-19 02:30:03 +0530

    Third commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <p1>Hello there !</p1>
+      </div>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Charges.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Charges = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Charges
+
+
+
+
+
